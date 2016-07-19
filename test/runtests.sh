843dda17bd239ea57328bc2d2b0c7e971763b87a
Spencer Russell <spencer.f.russell@gmail.com>
2016-07-19 18:53:01 -0400

    now trying with running the jack server and test script from a shell script

---
+++ test/runtests.sh
@@ -0,0 +1,7 @@
+ #!/bin/sh
+
+ # run the jack server in the background
+jackd -r -p24 -ddummy -r48000 -p2048 -w100000 &
+sleep 5
+julia -e 'Pkg.clone(pwd()); Pkg.build("JACKAudio"); Pkg.test("JACKAudio"; coverage=true)'
+killall jackd
